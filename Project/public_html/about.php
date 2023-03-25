f7c29f6d6a4518ef4ff2ce2ec166d819da6b404f
Marco <bozicma@gmail.com>
2023-03-24 20:25:39 -0400

    added student numbers

--- Project/public_html/about.php
+++ Project/public_html/about.php
@@ -13,11 +13,11 @@
 	        	<div class="col-sm-9">
                 <h1 class="page-header">Team Members</h1>
                 <ul>
-                    <li>Hodo Abdirizak</li>
-                    <li>Kevin Ahn</li>
-                    <li>Marco Bozic</li>
-                    <li>Mubariz Afzal</li>
-                    <li>Asgar Ayman</li>
+                    <li>Hodo Abdirizak (501029458)</li>
+                    <li>Kevin Ahn (500882444)</li>
+                    <li>Marco Bozic (500896651)</li>
+                    <li>Mubariz Afzal (500897653)</li>
+                    <li>Asgar Ayman (500976896)</li>
                 </ul>   
                 </div>
 	        </div>
