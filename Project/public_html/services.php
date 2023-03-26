8189e62467eb7f9c9b2f41eeda433f31890c965c
athkev <athkev@gmail.com>
2023-03-26 01:28:22 -0400

    shit forgot a file 3

---
+++ Project/public_html/services.php
@@ -0,0 +1,31 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition">
+<div class="wrapper">
+
+	<?php include 'includes/navbar.php'; ?>
+    <div class="content-wrapper">
+	    <div class="container">
+
+	    <!-- Main content -->
+	    <section class="content">
+	        <div class="row">
+	        	<div class="col-sm-9">
+                <h1 class="page-header">Admin Mode</h1>
+                <ul>
+                    <p>The Admin should be able to ‘Maintain” the system for all major transactions on the databases. TheAdmin can maintain the database to add /delete/search/edit the data. The Admin can maintain the database to cover alltypes of data: user accounts, login ids, passwords, titles, images, addresses, descriptions, orders, prices, invoices, user reviews, user rankings, latitudes, longitudes, addresses, ...</p>
+                    
+                </ul> 
+                </div>
+	        </div>
+        </section>
+	     
+	    </div>
+	</div>
+
+<?php include 'includes/footer.php'; ?>
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+</body>
+</html>
