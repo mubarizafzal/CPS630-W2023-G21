bab2d536ca3b7de9e9f76a66a026cb1f7796c2ec
hodoabdirizak <hodo.abdirizak@hotmail.com>
2023-04-07 15:36:18 -0400

    Changes to database

--- Project/public_html/database/iteration2.sql
+++ Project/public_html/database/iteration2.sql
@@ -19,17 +19,16 @@ CREATE TABLE IF NOT EXISTS `user` (
   `address` text NOT NULL,
   `citycode` text NOT NULL,
   `balance` float DEFAULT 0,
-  `admin` tinyint(1),
+  `admin` tinyint(1) DEFAULT 0,
   PRIMARY KEY(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 
 -- --------------------------------------------------------
 --
 -- Table structure for table `order_placed`
 --
 
-CREATE TABLE IF NOT EXISTS `order_placed` (
+CREATE TABLE IF NOT EXISTS `order` (
   `id` int(11) AUTO_INCREMENT NOT NULL,
   `user_id` int(11) NOT NULL,
   `shopping_cart_id` int(11) NOT NULL,
@@ -44,14 +43,14 @@ CREATE TABLE IF NOT EXISTS `order_placed` (
 
 -- Insert data in table 'order_placed'
 
-INSERT INTO `order_placed` (`id`, `user_id`, `shopping_cart_id`, `item_id`, `trip_id`, `date_issued`, `date_received`, `total`, `pay_code`) VALUES
+INSERT INTO `order` (`id`, `user_id`, `shopping_cart_id`, `item_id`, `trip_id`, `date_issued`, `date_received`, `total`, `pay_code`) VALUES
 (1, 1, 1, 1 ,1, 2023-02-06, 2023-03-16, 197, 'PAY01'),
 (2, 2, 2, 2 ,2, 2023-01-09, 2023-02-15, 197, 'PAY02'),
 (3, 3, 3, 3 ,3, 2023-12-12, 2023-12-30, 197, 'PAY03');
 
 -- --------------------------------------------------------
 --
--- Table structure for table `shopping_cart`
+-- Table structure for table `cart`
 --
 
 CREATE TABLE IF NOT EXISTS `shopping_cart` (
@@ -115,23 +114,23 @@ CREATE TABLE IF NOT EXISTS `items` (
 -- Insert data in table 'items'
 INSERT INTO `items` (`id`, `category_id`, `item_name`, `keyword`, `dept_code`, `made_in`, `price`, `description`, `code`, `photo`, `counter`) VALUES
 (1, 1, 'MALM', 'bed', 001, 'Canada', 329, '<p>A clean design with solid wood veneer. Place the bed on its own or with the headboard against a wall. If you need space for extra bedding, add MALM bed storage boxes on casters.</p>\r\n', 'malm-bed-frame-high-black-brown', 'malm-bed-frame-high-black-brown.jpg', '2'),
-(2, 1, 'KLEPPSTAD', 'bed', 001, 'Canada', 179, '<p>KLEPPSTAD has a stylish and modern design with a metal bed frame and a cozy textile headboard. Easy to both buy and take home since it’s sold as two compact packages. So practical and convenient!</p>\r\n', 'kleppstad-bed-frame-white-vissle-beige', 'kleppstad-bed-frame-white-vissle-beige.jpg', '4'),
+(2, 1, 'KLEPPSTAD', 'bed', 001, 'Canada', 179, '<p>KLEPPSTAD has a stylish and modern design with a metal bed frame and a cozy textile headboard. Easy to both buy and take home since it`s sold as two compact packages. So practical and convenient!</p>\r\n', 'kleppstad-bed-frame-white-vissle-beige', 'kleppstad-bed-frame-white-vissle-beige.jpg', '4'),
 (3, 1, 'SONGESAND', 'bed', 001, 'Canada', 259, '<p>A sturdy bed frame with soft, profile edges and high legs. A classic shape that will last for many years. Add SONGESAND bed underbed storage boxes to store extra bedding without taking up more space.</p>\r\n', 'songesand-bed-frame-white', 'songesand-bed-frame-white.jpg', '10'),
-(4, 1, 'SLATTUM', 'bed', 001, 'Canada', 219, '<p>Upholstered in soft woven fabric that brings a cozy feeling into your bedroom. The headboard is a comfy backrest for late night reading. And what’s more, it all comes in 1 package. Convenient, right?</p>\r\n', 'slattum-upholstered-bed-frame-knisa-light-gray', 'slattum-upholstered-bed-frame-knisa-light-gray.jpg', '4'),
+(4, 1, 'SLATTUM', 'bed', 001, 'Canada', 219, '<p>Upholstered in soft woven fabric that brings a cozy feeling into your bedroom. The headboard is a comfy backrest for late night reading. And what`s more, it all comes in 1 package. Convenient, right?</p>\r\n', 'slattum-upholstered-bed-frame-knisa-light-gray', 'slattum-upholstered-bed-frame-knisa-light-gray.jpg', '4'),
 (5, 1, 'BRIMNES', 'bed', 001, 'Canada', 529, '<p>A bed frame with hidden storage in several places – perfect if you live in a small space. The BRIMNES series has several smart solutions that help you save space.</p>\r\n', 'brimnes-bed-frame-with-storage-headboard-black', 'brimnes-bed-frame-with-storage-headboard-black.jpg', '2'),
 (6, 2, 'KIVIK', 'sofa', 001, 'Canada', 1599, '<p>Cuddle up in the comfortable KIVIK sofa. The generous size, low armrests and pocket springs with foam that adapts to the body invites you and your guests to many hours of socialising and relaxation.</p>\r\n', 'kivik-loveseat-with-chaise-tallmyra-white-black', 'kivik-loveseat-with-chaise-tallmyra-white-black.jpg', '4'),
 (7, 2, 'GLOSTAD', 'sofa', 001, 'Canada', 249, '<p>It should be easy to get a sofa and GLOSTAD sofa is easy to buy, bring home, assemble and live with. So you can enjoy more time and space to hang out with friends and family and do other important things.</p>\r\n', 'glostad-loveseat-knisa-dark-gray', 'glostad-loveseat-knisa-dark-gray.jpg', '2'),
-(8, 2, 'LINANAS', 'sofa', 001, 'Canada', 399, '<p>This cover is made from Vissle fabric in polyester, which is dope-dyed. It’s a durable material with a smooth weave and a nice two-tone effect.</p>\r\n', 'linanaes-sofa-vissle-dark-gray', 'linanaes-sofa-vissle-dark-gray.jpg', '4'),
-(9, 2, 'MORABO', 'sofa', 001, 'Canada', 899, '<p>Warm and welcoming, neat and stylish. The supporting seat cushions, the cover’s soft finish and the tight fit gives this sofa a perfect balance between its comfort, functions and look.</p>\r\n', 'morabo-sofa-gunnared-dark-gray-wood', 'morabo-sofa-gunnared-dark-gray-wood.jpg', '2'),
-(10, 2, 'FRIHETEN', 'sofa', 001, 'Canada', 1199, '<p>After a good night’s sleep, you can effortlessly convert your bedroom or guest room into a living room again. The built-in storage is easy to access and spacious enough to store bedding, books and PJs.</p>\r\n', 'friheten-corner-sofa-bed-with-storage-skiftebo-dark-gray', 'friheten-corner-sofa-bed-with-storage-skiftebo-dark-gray.jpg', '4'),
+(8, 2, 'LINANAS', 'sofa', 001, 'Canada', 399, '<p>This cover is made from Vissle fabric in polyester, which is dope-dyed. It`s a durable material with a smooth weave and a nice two-tone effect.</p>\r\n', 'linanaes-sofa-vissle-dark-gray', 'linanaes-sofa-vissle-dark-gray.jpg', '4'),
+(9, 2, 'MORABO', 'sofa', 001, 'Canada', 899, '<p>Warm and welcoming, neat and stylish. The supporting seat cushions, the cover`s soft finish and the tight fit gives this sofa a perfect balance between its comfort, functions and look.</p>\r\n', 'morabo-sofa-gunnared-dark-gray-wood', 'morabo-sofa-gunnared-dark-gray-wood.jpg', '2'),
+(10, 2, 'FRIHETEN', 'sofa', 001, 'Canada', 1199, '<p>After a good night`s sleep, you can effortlessly convert your bedroom or guest room into a living room again. The built-in storage is easy to access and spacious enough to store bedding, books and PJs.</p>\r\n', 'friheten-corner-sofa-bed-with-storage-skiftebo-dark-gray', 'friheten-corner-sofa-bed-with-storage-skiftebo-dark-gray.jpg', '4'),
 (11, 3, 'KALLAX', 'shelf', 001, 'Canada', 109, '<p>Standing or lying – the KALLAX series adapts to taste, space, needs and budget. Smooth surfaces and rounded corners give a feel of quality and you can personalize the shelving unit with inserts and boxes.</p>\r\n', 'kallax-shelf-unit-white', 'kallax-shelf-unit-white.jpg', '10'),
 (12, 3, 'BAGGEBO', 'shelf', 001, 'Canada', 219, '<p>Design with clean lines that matches your existing decor or other items in the BAGGEBO storage series. The neat look, with shelves and a back panel in metal and mesh, makes eye-catchers of what you store.</p>\r\n', 'baggebo-shelf-unit-metal-white', 'baggebo-shelf-unit-metal-white.jpg', '4'),
 (13, 3, 'LAIVA', 'shelf', 001, 'Canada', 39, '<p>Open storage is just as much about reflecting who you are as organizing all your things. In this bookcase, you can display your favourite books and objects. The shallow depth is perfect for smaller spaces.</p>\r\n', 'laiva-bookcase-black-brown', 'laiva-bookcase-black-brown.jpg', '2'),
-(14, 3, 'BILLY', 'shelf', 001, 'Canada', 249, '<p>It is estimated that every five seconds, one BILLY bookcase is sold somewhere in the world. Pretty impressive considering we launched BILLY in 1979. It’s the booklovers choice that never goes out of style.</p>\r\n', 'billy-bookcase-with-glass-doors-dark-blue', 'billy-bookcase-with-glass-doors-dark-blue.jpg', '4'),
-(15, 4, 'SANDSBERG', 'desk', 001, 'Canada', 59.99, '<p>This table for 4 blends a warm wood expression with sturdy metal in a slim design that’s pleasing to the eye even in smaller spaces. Pair it with SANDSBERG chair to create a welcoming and coordinated look.</p>\r\n', 'sandsberg-table-black', 'sandsberg-table-black.jpg', '2'),
+(14, 3, 'BILLY', 'shelf', 001, 'Canada', 249, '<p>It is estimated that every five seconds, one BILLY bookcase is sold somewhere in the world. Pretty impressive considering we launched BILLY in 1979. It`s the booklovers choice that never goes out of style.</p>\r\n', 'billy-bookcase-with-glass-doors-dark-blue', 'billy-bookcase-with-glass-doors-dark-blue.jpg', '4'),
+(15, 4, 'SANDSBERG', 'desk', 001, 'Canada', 59.99, '<p>This table for 4 blends a warm wood expression with sturdy metal in a slim design that`s pleasing to the eye even in smaller spaces. Pair it with SANDSBERG chair to create a welcoming and coordinated look.</p>\r\n', 'sandsberg-table-black', 'sandsberg-table-black.jpg', '2'),
 (16, 4, 'LINNMON', 'desk', 001, 'Canada', 74, '<p>Mix and match your choice of table top and legs – or choose this ready-made combination. Strong and light-weight, made with a technique that uses less raw materials, reducing the impact on the environment.</p>\r\n', 'linnmon-adils-table-white', 'linnmon-adils-table-white.jpg', '4'),
 (17, 4, 'MICKE', 'desk', 001, 'Canada', 159, '<p>A clean and simple look that fits just about anywhere. You can combine it with other desks or drawer units in the MICKE series to extend your work space. The clever design at the back hides messy cables.</p>\r\n', 'micke-desk-white', 'micke-desk-white.jpg', '2'),
-(18, 4, 'LAGKAPTEN', 'desk', 001, 'Canada', 358, '<p>Limited space doesn’t mean you have to say no to studying or working from home. This desk takes up little floor space yet still has two drawer units where you can store papers and other things.</p>\r\n', 'lagkapten-alex-desk-white', 'lagkapten-alex-desk-white.jpg', '4');
+(18, 4, 'LAGKAPTEN', 'desk', 001, 'Canada', 358, '<p>Limited space doesn`t mean you have to say no to studying or working from home. This desk takes up little floor space yet still has two drawer units where you can store papers and other things.</p>\r\n', 'lagkapten-alex-desk-white', 'lagkapten-alex-desk-white.jpg', '4');
 
 -- --------------------------------------------------------
 
@@ -270,11 +269,11 @@ INSERT INTO `truck` (`id`, `item_name`, `avail_code`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `payment` (
-`id` int(11) NOT NULL,
+`id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
   `card_no` text NOT NULL,
   `salt` text NOT NULL,
   `exp_year` text NOT NULL,
   `cvc` text NOT NULL,
   PRIMARY KEY(id)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
